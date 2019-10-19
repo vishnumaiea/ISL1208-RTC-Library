ef559c46c23f26adea394c3cc7bf83b29f3e5739
Vishnu M Aiea <vishnumaiea@gmail.com>
2019-10-19 21:05:47 +0530

    Added support for setting day of the week

--- src/ISL1208_RTC.h
+++ src/ISL1208_RTC.h
@@ -8,14 +8,14 @@
 //                                                                        //
 //  Filename : ISL1208_RTC.h                                              //
 //  Description : Header file for ISL1208_RTC library.                    //
-//  Library version : 1.4.2                                               //
+//  Library version : 1.4.3                                               //
 //  Author : Vishnu M Aiea                                                //
 //  Source : https://github.com/vishnumaiea/ISL1208-RTC-Library           //
 //  Author's website : www.vishnumaiea.in                                 //
 //  Initial release : IST 11:49:42 AM, 27-05-2018, Sunday                 //
 //  License : MIT                                                         //
 //                                                                        //
-//  File last modified : IST 11:14 AM 25-05-2019, Saturday                //
+//  File last modified : IST 09:04 PM 19-10-2019, Saturday                //
 //                                                                        //
 //========================================================================//
 
